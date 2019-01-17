9f9e3df6748717db9a6b6495bd3e7854c65b75d1
Wes Billman <wes@compass.com>
2019-01-17 11:30:20 -0500

    Convert project to swift/kotlin

--- ios/Classes/StoragePlugin.m
+++ ios/Classes/StoragePlugin.m
@@ -1,20 +1,8 @@
 #import "StoragePlugin.h"
+#import <storage/storage-Swift.h>
 
 @implementation StoragePlugin
 + (void)registerWithRegistrar:(NSObject<FlutterPluginRegistrar>*)registrar {
-  FlutterMethodChannel* channel = [FlutterMethodChannel
-      methodChannelWithName:@"storage"
-            binaryMessenger:[registrar messenger]];
-  StoragePlugin* instance = [[StoragePlugin alloc] init];
-  [registrar addMethodCallDelegate:instance channel:channel];
+  [SwiftStoragePlugin registerWithRegistrar:registrar];
 }
-
-- (void)handleMethodCall:(FlutterMethodCall*)call result:(FlutterResult)result {
-  if ([@"getPlatformVersion" isEqualToString:call.method]) {
-    result([@"iOS " stringByAppendingString:[[UIDevice currentDevice] systemVersion]]);
-  } else {
-    result(FlutterMethodNotImplemented);
-  }
-}
-
 @end
